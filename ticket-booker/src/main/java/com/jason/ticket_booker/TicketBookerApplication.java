9b0de36eee5261f9e488e7ad832e319c74f8b0c9
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-07 07:13:25 +0530

    Initial commit

---
+++ ticket-booker/src/main/java/com/jason/ticket_booker/TicketBookerApplication.java
@@ -0,0 +1,14 @@
+package com.jason.ticket_booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookerApplication.class, args);
+
+	}
+
+}
---
+++ ticket-booker/src/test/java/com/jason/ticket_booker/TicketBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.ticket_booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
