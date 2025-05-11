103974a67962e7efb1d98caf10c0b22350e078e8
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-11 21:55:00 +0530

    Create UserNotFoundException.java

---
+++ ticket-booker/src/main/java/com/jason/ticket_booker/exceptions/UserNotFoundException.java
@@ -0,0 +1,23 @@
+package com.jason.ticket_booker.exceptions;
+
+public class UserNotFoundException extends EventTicketException{
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+    }
+}
