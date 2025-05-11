b60fe0177f95324b547a3b3a54594e18c5ca7fe3
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-11 21:16:47 +0530

    Create JpaConfiguration.java

---
+++ ticket-booker/src/main/java/com/jason/ticket_booker/config/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package com.jason.ticket_booker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+
+}
