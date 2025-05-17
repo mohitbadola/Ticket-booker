19aafff43f111a1c6570b8c5cfbe09d0e2a68053
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-17 19:32:25 +0530

    Create CreateEventRequestDto.java

---
+++ ticket-booker/src/main/java/com/jason/ticket_booker/domain/dtos/CreateEventRequestDto.java
@@ -0,0 +1,21 @@
+package com.jason.ticket_booker.domain.dtos;
+
+import com.jason.ticket_booker.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequestDto {
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+}
