6da1c633c9df7259610c752c61d04ddf465a7afe
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-05-17 20:01:39 +0530

    Create CreateEventResponseDto.java

---
+++ ticket-booker/src/main/java/com/jason/ticket_booker/domain/dtos/CreateEventResponseDto.java
@@ -0,0 +1,27 @@
+package com.jason.ticket_booker.domain.dtos;
+
+import com.jason.ticket_booker.domain.entities.EventStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventResponseDto {
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<CreateTicketTypeResponseDto> ticketTypes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
